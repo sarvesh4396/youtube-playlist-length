130f6ec1e187bdaa7f4f7419845fe12a67938b4e
sarvesh4396 <heysarvesh@pm.me>
2022-08-05 15:38:54 +0530

    add main func

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-
 from utils import get_id, get_data
+import uvicorn
+
 
 app = FastAPI()
 
@@ -30,3 +31,7 @@ def calculate(request: Request, url: str = Form("")):
     return templates.TemplateResponse(
         "index.html", context={"request": request, "final_text": final_text}
     )
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
